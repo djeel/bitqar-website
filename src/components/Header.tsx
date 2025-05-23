f291532197099c12b446cf21364d9a708c2d600e
djeel <49989313+djeel@users.noreply.github.com>
2025-05-23 23:29:46 +0200

    better header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-[80px] bg-white/50 backdrop-blur-[10px] border-b border-white border-b-[0.5px] z-50">
       {/* Ajoutez un conteneur pour limiter la largeur et centrer */}
-      <div className="ml-[430px] w-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-full">
+      <div className="mx-auto w-full max-w-[calc(100vw-860px)] px-4 sm:px-6 lg:px-8 flex justify-between items-center h-full">
         <div className="flex items-center h-full">
           <img src="/bitqar-logo.png" alt="Bitqar Logo" className="max-h-[40px] w-auto max-w-[180px]" />
         </div>
